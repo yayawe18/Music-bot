a6de0dfcf9d9d987f4cfdb095ab52bf76444397d
recon <66986299+reconlx@users.noreply.github.com>
2020-12-01 14:06:32 +0800

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -8,7 +8,6 @@ exports.run = async (client, message) => {
             fields: [
                 { name: 'Bot', value: '`ping`' },
                 { name: 'Music', value: '`play`, `pause`, `resume`, `queue`, `clear-queue`, `shuffle`, `np`, `loop`, `volume`, `skip`, `stop`, `filter`, `w-filters`' },
-                { name: 'Filters', value: '`bassboost`, `8D`, `vaporwave`, `nightcore`, `phaser`, `tremolo`, `vibrato`, `reverse`, `treble`, `normalizer`, `surrounding`, `pulsator`, `subboost`, `karaoke`, `flanger`, `gate`, `haas`, `mcompand`' },
             ],
             timestamp: new Date(),
             description: `To use filters, ${client.config.prefix}filter (the filter). Example : ${client.config.prefix}filter 8D.`,
