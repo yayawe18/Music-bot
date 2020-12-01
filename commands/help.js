d137cdca82a67b9460049f0dc0868336386e6a29
LimXuan7 <olimxuan2005@gmail.com>
2020-12-01 14:35:53 +0800

    second

--- commands/help.js
+++ commands/help.js
@@ -7,7 +7,7 @@ exports.run = async (client, message) => {
             footer: { text: 'github.com/reconlx/music-bot' },
             fields: [
                 { name: 'Bot', value: '`ping`' },
-                { name: 'Music', value: '`play`, `pause`, `resume`, `queue`, `clear-queue`, `shuffle`, `np`, `loop`, `volume`, `skip`, `stop`, `filter`, `w-filters`' },
+                { name: 'Music', value: '`play`, `pause`, `resume`, `queue`, `clear-queue`, `shuffle`, `np`, `loop`, `volume`, `skip`, `stop`' },
             ],
             timestamp: new Date(),
             description: `To use filters, ${client.config.prefix}filter (the filter). Example : ${client.config.prefix}filter 8D.`,
